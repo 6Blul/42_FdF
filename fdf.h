029beb918c6ce1755ebbdf0ca4a13ead2cba7e38
Sophie POCHEZ <spochez@e2r3p14.42.fr>
2014-12-11 06:33:55 +0100

    le .c et le .h psk la vie

---
+++ fdf.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/11 04:59:22 by spochez           #+#    #+#             */
+/*   Updated: 2014/12/11 05:50:49 by spochez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//Il est si sympa que vous n'en reviendrez pas votre ami Pitty le Panda
+
+#ifndef FDF_H
+# define FDF_H
+
+typedef struct		s_pd
+{
+	double x;
+	double y;
+	double z;
+}					t_pd;
+
+typedef struct		s_con
+{
+	int	i;
+	int	j;
+	int	k;
+}					t_con;
+
+#endif
