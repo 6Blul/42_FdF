a6db28d9241de22c7163829864438677a85bf9ab
Sophie POCHEZ <spochez@e3r3p10.42.fr>
2014-12-13 00:57:52 +0100

    je oui

--- FdF_get.c
+++ FdF_get.c
@@ -6,32 +6,52 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/11 05:27:49 by spochez           #+#    #+#             */
-/*   Updated: 2014/12/11 06:26:02 by spochez          ###   ########.fr       */
+/*   Updated: 2014/12/13 00:57:39 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <mlx.h>
 #include <stdlib.h>
 
-t_pd	***get_nbr(char *file)
+t_coor	***get_nbr(int fd)
 {
 	char			*oui;
-	char			**gato;
-	struct	t_pd	***cake;
-	struct	t_con	tent;
-	int				fd;
+	static char		**split;
+	t_coor			***pt;
+	t_con			tent;
 
-	tent.i = 1;
-	tent.j = 0;
-	fd = open(file, O_RDONLY);
-	while (get_next_line(fd, &oui) > 0)
+	if (tent.j == 0)
 	{
-		cake.x = tent.i;
+		get_next_line(fd, &oui);
+		split = ft_strsplit(oui, ' ');
+	}
+	if (oui == NULL)
+	{
+		pt = NULL;
+		return (pt);
+	}
+	pt.x = tent.i;
+	pt.z = ft_atoi(split[tent.j]);
+	pt.y = tent.j;
+	if (!split[tent.j + 1])
+	{
+		tent.j = 0;
 		tent.i++;
-		gato = ft_strsplit(&oui, ' ');
-		while (*gato)
-		{
-				
-		}
+	}
+	else
+		tent.j++;
+	return (pt);
+}
+
+void	treat_nbr(char *file)
+{
+	int			fd;
+	t_coor		***pt;
+
+	fd = open(file, O_RDONLY);
+	while (pt != NULL)
+	{
+		pt = get_nbr(fd);
 	}
 }
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/11 04:59:22 by spochez           #+#    #+#             */
-/*   Updated: 2014/12/11 05:50:49 by spochez          ###   ########.fr       */
+/*   Updated: 2014/12/12 23:29:59 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 //Il est si sympa que vous n'en reviendrez pas votre ami Pitty le Panda
@@ -14,18 +14,20 @@
 #ifndef FDF_H
 # define FDF_H
 
-typedef struct		s_pd
+typedef struct		s_coor
 {
-	double x;
-	double y;
-	double z;
-}					t_pd;
+	double 		x;
+	double 		y;
+	double 		z;
+}					t_coor;
 
 typedef struct		s_con
 {
-	int	i;
-	int	j;
-	int	k;
+	static int	i;
+	static int	j;
 }					t_con;
+
+t_pd	***get_nbr(int fd);
+void	treat_nbr(char *file);
 
 #endif
