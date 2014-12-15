2948e927aad1af434db2240456804ec8fbfd57ff
Sophie POCHEZ <spochez@e3r3p14.42.fr>
2014-12-15 23:10:10 +0100

    Reac

--- fdf.h
+++ fdf.h
@@ -6,11 +6,9 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/11 04:59:22 by spochez           #+#    #+#             */
-/*   Updated: 2014/12/13 04:30:59 by spochez          ###   ########.fr       */
+/*   Updated: 2014/12/15 23:09:58 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//Il est si sympa que vous n'en reviendrez pas votre ami Pitty le Panda
-//Quand mon programme y marche pas... ca me rend treeeees triste.
 
 #ifndef FDF_H
 # define FDF_H
@@ -22,7 +20,6 @@ typedef struct		s_coor
 	double 		z;
 }					t_coor;
 
-t_coor	***get_nbr(const int fd);
 void	treat_nbr(char *file);
 
 #endif
