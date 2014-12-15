7bfe22d87298487463948dfa91291cf82f2d370b
Sophie POCHEZ <spochez@e3r3p12.42.fr>
2014-12-15 01:51:50 +0100

    ca me rend tres triste

--- FdF_get.c
+++ FdF_get.c
@@ -6,49 +6,50 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/13 02:50:39 by spochez           #+#    #+#             */
-/*   Updated: 2014/12/13 04:36:44 by spochez          ###   ########.fr       */
+/*   Updated: 2014/12/15 01:38:21 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include "fdf.h"
+#include "../Libft/includes/libft.h"
 
-void	get_coors(char **split, t_coor ****tab)
+
+void	fill_struct(char **split, t_coor ****pts)
 {
 	int		i;
 
 	i = 0;
 	while (split[i])
 	{
-		tab[y][x]->y = i;
-		tab->z = ft_atoi(*tab[x][y]);
-		**tab++;
+		pts->z = ft_atoi(split[i]);
+		pts->y = i;
 		i++;
 	}
 }
 
-void	get_nbr(const int fd, t_coor ****tab)
+void	get_nbr(int fd)
 {
+	t_coor	***pts;
 	char	*buff;
 	char	**split;
 
-	tab->x = 0;
+	pts.x = 0;
 	while (get_next_line(fd, &buff) > 0)
 	{
-		split = ft_strsplit(buff);
-		get_coors(split, &tab);
-		*tab++;
-		tab->x++;
+		split = ft_strsplit(buff, ' ');
+		fill_struct(split, &pts);
+		pts.x++;
 	}
 }
 
-t_coor	***treat_nbr(char *file)
+void	treat_nbr(char *file)
 {
-	const int	fd;
-	t_coor		***tab;
+	int		fd;
 
 	fd = open(file, O_RDONLY);
-	get_nbr(fd, &tab);
+	get_nbr(fd);
 }
